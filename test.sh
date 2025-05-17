8207eea0b790eff036a20309d24766221c06c923
Linux Machine <illyasmado@outlook.com>
2025-05-17 19:43:16 +0100

    Removed a test script.

--- test.sh
+++
@@ -1,9 +0,0 @@
-first="carrot"
-second="banana"
-
-if [[ "$first" > "$second" ]]
-then 
-	echo "$first comes before $second alphabetically"
-else
-	echo "$second comes after $first alphabetically"
-fi
