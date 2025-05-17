8dc62ab890689fdbe2bed307f0970ed71770629d
Linux Machine <illyasmado@outlook.com>
2025-05-17 09:59:45 +0100

    Removed remaining copies of scripts.

--- my-first-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This script will greet the user using the name specified in the $USER variable.
-echo "Welcome $USER!"
--- parameters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod +x parameters.sh
-
-echo "First parameter you passed: $1"
-echo "Second parameter you passed: $2"
-echo ""
-echo "All parameters that were passed v"
-echo $@
