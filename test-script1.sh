9be8f5d5068e07c2fd1f5d6b0a41498abb41513d
Windows PC <illyasmado@outlook.com>
2025-05-20 13:31:45 +0100

    Test

--- test-script1.sh
+++ test-script1.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
-set euo pipefail
+set -euo pipefail
+
+recheck=0
 
 echo "Enter the directory to append to PATH variable. (Enter . to use the current directory.)"
 read directory
 
-check () {
+checkPath() {
 
+	grep "$directory" ~/.bashrc
+	if [ $? -eq 0 ]; then
+		if [ "$recheck" -eq 0 ]; then
+			echo "Directory already exists in the path variable."
+			exit 0
+		elif [ "$recheck" -eq 1 ]; then
+			echo "Directory has been added to the path variable."
+			exit 0
+		fi
+	elif [ "$recheck" -eq 1 ]; then
+			echo "Failed to add the directory to path variable."
+			exit 1
+	else
+		return
+	fi
+}
 
+addDir () {
+
+	echo "export PATH=$PATH:~/$1" >> ~/.bashrc
+	source ~/.bashrc
+	recheck=1
 
 }
 
-if [ directory = "." ]; then
-	currentDir=$(pwd)
-	echo "export PATH=$PATH:~/$currentDir" >> ~/.bashrc
-        source ~/.bashrc
-	echo "$?"
+if [ "$directory" = "." ]; then
+	directory=$(pwd)
+	checkPath
+	addDir
+	checkPath
 elif [ -d "$directory" ]; then
-	grep "$directory" ~/.bashrc
-	if [ "$?" -eq  1 ]; then
-		echo "export PATH=$PATH:~/$directory" >> ~/.bashrc
-        	source ~/.bashrc
-		echo "Success: Directory has been added to the path variable."
-	else
-		echo "Fail: Directory already appended to the path variable." 
+	checkPath
+	addDir
+	checkPath
 else
 	echo "Directory does not exist."
 	exit 1
