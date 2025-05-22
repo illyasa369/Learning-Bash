0499b5db7c893f6b3f682ad3353b50823ba0f7b3
Linux Machine <illyasmado@outlook.com>
2025-05-22 15:00:19 +0100

    Reads from the file specified and prints to the terminal.

---
+++ read-file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set euo pipefail
+
+
+echo "Enter the path of the file you wish to read from."
+read filePath
+
+if [ ! -e "$filePath" ]; then
+	echo "File not found. Check your file path."
+else
+	while IFS= read -r line
+	do 
+		echo "$line"
+	done < "$filePath"
+fi
