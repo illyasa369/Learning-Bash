7d45f3de2bc623bc1c91385ddfe0959202e49542
Linux Machine <illyasmado@outlook.com>
2025-05-28 15:50:42 +0100

    Fixes issue with processing empty line.

--- bash-battle-arena/level-13.sh
+++ bash-battle-arena/level-13.sh
@@ -25,19 +25,21 @@ backupDirectory () {
 	mkdir -p "$directoryName"_backups
 
 	backupCount=$(ls "$directoryName"_backups | wc -l)
+
 	if [ "$backupCount" -eq 5 ]
 	then
 		echo -e "Backup limit reached.\nRemoving oldest backup."
-		oldestBackup=$(ls -l "$directoryName"_backups | sort -k 6,8 | awk '{print $9}' | head -n 1)
+		oldestBackup=$(ls -l "$directoryName"_backups | sort -k 6,8 | awk '{print $9}' | sed -n '2p')
 		rm -v "$directoryName"_backups/"$oldestBackup"
 		tar -czf "$directoryName"_backups/backup_$(date +%Y%m%d_%H%M%S).tar.gz "$1"
-		echo "Backup created."
+		echo "New backup created."
 	else
-		echo "here"
 		tar -czf "$directoryName"_backups/backup_$(date +%Y%m%d_%H%M%S).tar.gz "$1"
-		echo "Backup created."
+		echo "New backup created."
 	fi
+
 }
+
 
 echo "Enter the path of the directory you wish to enable backups for:"
 read searchDir
